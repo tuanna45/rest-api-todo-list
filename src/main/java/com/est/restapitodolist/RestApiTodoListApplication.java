b67f4adb2bab97bb2a0ce5a459279172b6c124ce
Tuan Ngo <tuan.ngoanh@axonactive.vn>
2019-11-12 14:33:14 +0700

    Initial commit

---
+++ src/main/java/com/est/restapitodolist/RestApiTodoListApplication.java
@@ -0,0 +1,12 @@
+package com.est.restapitodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiTodoListApplication.class, args);
+    }
+}
---
+++ src/test/java/com/est/restapitodolist/RestApiTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.restapitodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
